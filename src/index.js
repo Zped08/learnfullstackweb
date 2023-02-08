fc6768fd0316450bd8312a03e7725597b80a8810
Jair Flores <jairflores0802@gmail.com>
2023-02-08 13:16:46 +0100

    guia telefonica con campo de busqueda

--- src/index.js
+++ src/index.js
@@ -4,17 +4,29 @@ import ReactDOM from "react-dom";
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: 213213131 },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
 
   const handleName = (event) => {
     setNewName(event.target.value);
   };
-  
+
   const handleNumber = (event) => {
     setNewNumber(event.target.value);
   };
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   const addName = (event) => {
     event.preventDefault();
@@ -25,13 +37,16 @@ const App = () => {
     event.preventDefault();
     setPersons(persons.concat({ name: newName, number: newNumber }));
     setNewName("");
+    setNewNumber("");
   };
-
-
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter show with <input value={searchTerm} onChange={handleSearch} />
+      </div>
+      <h2>Add new contact</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleName} />
@@ -46,7 +61,7 @@ const App = () => {
 
       <h2>Numbers</h2>
       <div>
-        {persons.map((person) => (
+        {filteredPersons.map((person) => (
           <p key={person.name}>
             {person.name} {person.number}
           </p>
